d1ca98fdac999d4d3badc253f54a18ae58de30c9
ccyy <15016250640@163.com>
2020-12-15 19:07:48 +0800

    20201215

---
+++ 738.单调递增的数字.java
@@ -0,0 +1,28 @@
+ /*
+ * @lc app=leetcode.cn id=738 lang=java
+ *
+ * [738] 单调递增的数字
+ */
+
+// @lc code=start
+class Solution {
+    public int monotoneIncreasingDigits(int N) {
+        char[] strN = Integer.toString(N).toCharArray();
+        int i = 1;
+        while (i < strN.length && strN[i - 1] <= strN[i]) {
+            i += 1;
+        }
+        if (i < strN.length) {
+            while (i > 0 && strN[i - 1] > strN[i]) {
+                strN[i - 1] -= 1;
+                i -= 1;
+            }
+            for (i += 1; i < strN.length; ++i) {
+                strN[i] = '9';
+            }
+        }
+        return Integer.parseInt(new String(strN));
+    }
+}
+// @lc code=end
+
